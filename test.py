67b17ea3838a1139ffc5b0a4a860dd943ecbe6dc
Timothy Pulliam <tpulliam@atcc.org>
2025-12-11 17:48:26 +0000

    sysinfo test layout

--- test.py
+++ test.py
@@ -1,80 +1,88 @@
+from __future__ import annotations
+
+
 from textual.app import App, ComposeResult
 from textual.widgets import TabbedContent, TabPane, Static
 from textual.containers import Vertical, Horizontal, Grid
 
 text = """Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."""
 
-class ExampleApp(App):
+from textual.containers import Container, Grid, VerticalScroll, ScrollableContainer 
+from textual.app import App
+from textual.widgets import Header, Footer, TabbedContent, TabPane, Tab, Tabs, Sparkline
+from textual.events import Resize
+from textual.reactive import reactive
 
+
+class Test(App):
     CSS_PATH = "test.tcss"
+
+    text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+
+    #width = reactive(0)
     def compose(self) -> ComposeResult:
+
+        self.l1 = VerticalScroll(id="l1", classes="l1", can_focus=True)
+        self.l2 = VerticalScroll(id="l2", classes="l2", can_focus=True)
+
+
+        yield Header()
         with TabbedContent():
+            with TabPane("one"):
+                with self.l1:
+                    c1 = VerticalScroll (id="c1", can_focus=True)
+                    c1.border_title = "c1"
+                    with c1:
+                        yield Static(text)
+                        yield Sparkline(data=[1,2,3,4,5,4,3,2,1])
+                    c2 = VerticalScroll (id="c2", can_focus=True)
+                    c2.border_title = "c2"
+                    with c2:
+                        yield Static(text)
+                        yield Sparkline(data=[1,2,3,4,5,4,3,2,1])
+                    c3 = VerticalScroll (id="c3", can_focus=True)
+                    c3.border_title = "c3"
+                    with c3:
+                        yield Static(text)
+                    c4 = VerticalScroll (id="c4", can_focus=True)
+                    c4.border_title = "c4"
+                    with c4:
+                        yield Static(text)
+            with TabPane("two"):
+                with self.l2:
+                    c1 = VerticalScroll (id="c1", can_focus=True)
+                    c1.border_title = "c1"
+                    with c1:
+                        yield Static(text)
+                        yield Sparkline(data=[1,2,3,4,5,4,3,2,1])
+                    c2 = VerticalScroll (id="c2", can_focus=True)
+                    c2.border_title = "c2"
+                    with c2:
+                        yield Static(text)
+                        yield Sparkline(data=[1,2,3,4,5,4,3,2,1])
+                    c3 = VerticalScroll (id="c3", can_focus=True)
+                    c3.border_title = "c3"
+                    with c3:
+                        yield Static(text)
+                    c4 = VerticalScroll (id="c4", can_focus=True)
+                    c4.border_title = "c4"
+                    with c4:
+                        yield Static(text)
+        yield Footer()
 
-            with TabPane('a'):
-                yield Static('1', classes="box")
-                yield Static('2', classes="box")
-                yield Static('3', classes="box")
+    def on_resize(self, event: Resize) -> None:
+        width = event.size[0]
+        if width <= 80: 
+            self.l1.remove_class("l1")
+            self.l2.remove_class("l2")
+            self.l1.add_class("l1-vertical")
+            self.l2.add_class("l2-vertical")
+        else:
+            self.l1.remove_class("l1-vertical")
+            self.l2.remove_class("l2-vertical")
+            self.l1.add_class("l1")
+            self.l2.add_class("l2")
 
-            with TabPane('b'):
-                with Vertical():
-                    yield Static('1', classes="box")
-                    yield Static('2', classes="box")
-                    yield Static('3', classes="box")
-
-            with TabPane('c'):
-                with Horizontal():
-                    yield Static('1', classes="box")
-                    yield Static('2', classes="box")
-                    yield Static('3', classes="box")
-
-            with TabPane('d'):
-                with Vertical():
-                    with Horizontal():
-                        yield Static('1', classes="box")
-                        yield Static('2', classes="box")
-                    with Horizontal():
-                        yield Static('3', classes="box")
-                        yield Static('4', classes="box")
-
-            with TabPane('e'):
-                with Grid(id="grid"):
-                    yield Static(text, classes="grid-box")
-                    yield Static(text, classes="grid-box")
-                    yield Static(text, classes="grid-box")
-                    yield Static(text, classes="grid-box")
-                    yield Static(text, classes="grid-box")
-                    yield Static(text, classes="grid-box")
-if __name__ == '__main__':
-    app = ExampleApp()
+if __name__ == "__main__":
+    app = Test()
     app.run()
-
-# from textual.app import App, ComposeResult
-# from textual.containers import Container, Horizontal, VerticalScroll
-# from textual.widgets import Header, Static
-
-
-# class CombiningLayoutsExample(App):
-#     CSS_PATH = "test.tcss"
-
-#     def compose(self) -> ComposeResult:
-#         yield Header()
-#         with Container(id="app-grid"):
-#             with VerticalScroll(id="left-pane"):
-#                 for number in range(15):
-#                     yield Static(f"Vertical layout, child {number}")
-#             with Horizontal(id="top-right"):
-#                 yield Static("Horizontally")
-#                 yield Static("Positioned")
-#                 yield Static("Children")
-#                 yield Static("Here")
-#             with Container(id="bottom-right"):
-#                 yield Static("This")
-#                 yield Static("panel")
-#                 yield Static("is")
-#                 yield Static("using")
-#                 yield Static("grid layout!", id="bottom-right-final")
-
-
-# if __name__ == "__main__":
-#     app = CombiningLayoutsExample()
-#     app.run()
